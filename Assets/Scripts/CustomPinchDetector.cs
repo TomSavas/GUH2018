bf875349cbe0a5faec6a8d0ca0469cca66850e6b
Simas Kuprelis <simas.kuprelis@gmail.com>
2018-11-10 18:53:41 +0000

    Initial commit

---
+++ Assets/Scripts/CustomPinchDetector.cs
@@ -0,0 +1,24 @@
+﻿using Leap.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPinchDetector : PinchDetector {
+
+    public HandModelBase hand;
+
+	// Use this for initialization
+	void Start () {
+        HandModel = hand;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsPinching) Debug.Log("valiooo");
+	}
+
+    protected override void ensureUpToDate()
+    {
+        base.ensureUpToDate();
+    }
+}
