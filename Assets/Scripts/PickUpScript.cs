784c72ffaa50215037febfe75afa9ae9ec5e5baf
Laurynas Raudonius <laurynasraudonius@student.manchester.ac.uk>
2018-11-10 19:57:50 +0000

    Add Pick up prefabs and scripts

---
+++ Assets/Scripts/PickUpScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class PickUpScript : MonoBehaviour {
+
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.CompareTag ("Player")) {
+			Debug.Log ("Player");
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnPickUps.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnPickUps : MonoBehaviour {
+
+	public int spawnsPerMinute;
+	public GameObject pickUp;
+	private bool spawnedYet = false;
+
+	void Update(){
+		int time = (Mathf.RoundToInt(Time.time)) % (60/spawnsPerMinute);
+		if (!spawnedYet) {
+			int randomedTime = Random.Range (1, 60/spawnsPerMinute - 1);
+			if (time >= randomedTime) {
+				Instantiate (pickUp, generateNewPosition(), Quaternion.identity);
+				spawnedYet = true;
+			}
+		}
+		if (time == 0) {
+			spawnedYet = false;
+		}
+	}
+	Vector3 generateNewPosition(){
+		return new Vector3 (0, 0, 0);
+	}
+
+}
