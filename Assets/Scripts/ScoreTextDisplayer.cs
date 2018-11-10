38eac9b6ad71a572938b0a040dc72716f504408c
Sawas <tomas.savickas@protonmail.com>
2018-11-10 15:43:53 +0000

    Add Score display binder

---
+++ Assets/Scripts/ScoreTextDisplayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class ScoreTextDisplayer : MonoBehaviour
+{
+	public ScoringSystem ScoringSystem;
+	public String ScorePrefix;
+
+	private TextMeshProUGUI _textMesh;
+	
+	private void Start ()
+	{
+		_textMesh = GetComponent<TextMeshProUGUI>();
+	}
+	
+	private void Update ()
+	{
+		_textMesh.text = ScorePrefix + ScoringSystem.Score;
+	}
+}
