4b3c83a06541c6829347c8d0bdb155ced43aab76
Sawas <tomas.savickas@protonmail.com>
2018-11-10 22:50:27 +0000

    Remove unnecessary comment

--- Assets/Scripts/ScoringSystem.cs
+++ Assets/Scripts/ScoringSystem.cs
@@ -17,12 +17,10 @@ public class ScoringSystem : MonoBehaviour
 			Score += GetScoreIncrement();
 			_highScore = Mathf.Max(Score, _highScore);
 		}
-		
 	}
 
 	private float GetScoreIncrement()
 	{
-		// TODO: Increment depending on the amount of bees, should probably have an exponential (1.25) dependency on time.
 		return 1f;
 	}
 
