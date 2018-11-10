9aed84d47a513893eaaa3b5850e06a075719700e
Sawas <tomas.savickas@protonmail.com>
2018-11-10 22:49:49 +0000

    Add minimal menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+	public GameObject Player;
+	public GameObject Score;
+	public GameObject EnemySpawner;
+	public GameObject PickupSpawner;
+	public CameraFollower CameraMovement;
+	public ScoringSystem Scoring;
+
+	public GameObject Title;
+	public GameObject PlayButton;
+	public GameObject ExitButton;
+
+	private bool _isInGame;
+	
+	public void StartGame()
+	{
+		_isInGame = true;
+		Player.SetActive(true);
+		Score.SetActive(true);
+		//EnemySpawner.SetActive(true);
+		//PickupSpawner.SetActive(true);
+		CameraMovement.enabled = true;
+		Scoring.Playing = true;
+		Scoring.ResetScore();
+		
+		//Hide menu UI
+		Title.SetActive(false);
+		PlayButton.SetActive(false);
+		ExitButton.SetActive(false);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
