0974b424e4714557548f753a0bb9131731139247
Sawas <tomas.savickas@protonmail.com>
2018-11-11 08:05:42 +0000

    Add player bullet

---
+++ Assets/Scripts/PlayerBulletBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class PlayerBulletBehavior : MonoBehaviour {
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Enemy")
+		{
+			Destroy(other.gameObject);
+			GameObject.FindObjectOfType<ScoringSystem>().BigIncrement();
+		}
+		else if (other.tag == "Border")
+		{
+			Physics2D.IgnoreCollision(GetComponent<CircleCollider2D>(), other);
+			Destroy(gameObject, 0.5f);
+		}
+	}
+}
