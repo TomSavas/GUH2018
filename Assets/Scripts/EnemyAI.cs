db38ba8bf8fa25d2e4354fc79cb46983f901ef79
Laurynas Raudonius <laurynasraudonius@student.manchester.ac.uk>
2018-11-11 09:43:03 +0000

    Add EnemyAI scripts

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+	
+	public GameObject camera;
+	public float speed;
+
+	private GameObject _player;
+
+
+	private void Start() {
+		_player = GameObject.FindGameObjectWithTag ("Player");
+	}	
+
+	private void FixedUpdate () {
+		Debug.Log (_player.transform.position.x);
+		float distance = Mathf.Sqrt (
+			Mathf.Pow((_player.transform.position.x - transform.position.x), 2) + 
+			Mathf.Pow((_player.transform.position.y - 2 - transform.position.y), 2));
+		float sinA = (_player.transform.position.x - transform.position.x) /
+			(distance);
+		float cosA = (_player.transform.position.y - 2 - transform.position.y) /
+			(distance);
+		float speedX = speed * sinA * Time.deltaTime;
+		float speedY = (camera.GetComponent<CameraFollower>().speed + speed * cosA) * Time.deltaTime;
+		transform.position += 
+			new Vector3 (speedX, speedY, 0);
+	}
+}
