c43e782db8073abc1dfef494b3865563295a1991
Sawas <tomas.savickas@protonmail.com>
2018-11-11 07:07:40 +0000

    Add health and power powerups

---
+++ Assets/Scripts/PowerPickup.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPickup : PickUpScript {
+    protected override void DoPickupAction(GameObject player)
+    {
+        player.GetComponent<PlayerBehavior>().UpgradeBullets();
+    }
+}
